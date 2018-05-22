08c9d3bc59baaeab30f4d9e5d073f337fe208e22
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-23 01:32:15 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
+
 
 
 fs.readdir("./commands/", (err, files) => {
