7b2ad97d3977f8b77b5e289d77f11789fdac9314
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-25 18:55:20 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,11 +14,11 @@ fs.readdir("./commands/", (err, file) => {
     return;
   }
 
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
+//   jsfile.forEach((f, i) =>{
+//     let props = require(`./commands/${f}`);
+//     console.log(`${f} loaded!`);
+//     bot.commands.set(props.help.name, props);
+//   });
 
 });
 
@@ -34,23 +34,24 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-let coins = JSON.parse(fs.readFileSync("./coins.json", "utf8"));
-let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+ let coins = JSON.parse(fs.readFileSync("./coins.json", "utf8"));
+// let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
-if(!prefixes[message.guild.id]){
-  prefixes[message.guild.id] = {
-    prefixes: botconfig.prefix
-  };
+// if(!prefixes[message.guild.id]){
+//   prefixes[message.guild.id] = {
+//     prefixes: botconfig.prefix
+//  };
 }
 
-  let prefix = prefixes[message.guild.id].prefixes;
+//   let prefix = prefixes[message.guild.id].prefixes;
+ let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(message.content.indexOf(prefix) !== 0) return;
-  if(commandfile) commandfile.run(bot,message,args);
+//   let commandfile = bot.commands.get(cmd.slice(prefix.length));
+//   if(message.content.indexOf(prefix) !== 0) return;
+//   if(commandfile) commandfile.run(bot,message,args);
 
 
 
