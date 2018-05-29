c89302010c3dd8ed389633ec973b38392794336b
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-29 20:01:24 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const botconfig = require("./botconfig.json");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
-
 fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
+  if (err) console.log(err);
 
   let jsfile = files.filter(f => f.split(".").pop() === "js")
   if(jsfile.length <= 0){
@@ -16,11 +14,10 @@ fs.readdir("./commands/", (err, files) => {
   }
 
   jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-
+  let props = require(`./commands/${f}`);
+  console.log(`${f} loaded!`);
+  bot.commands.set(props.help.name, props);
+    });
 
 });
 
@@ -30,28 +27,18 @@ bot.on("ready", async () => {
   bot.user.setActivity("Grand Theft Auto San Andreas");
 });
 
-
 bot.on("message", async message => {
   if(message.author.bot) return;
-  if(message.channel.type === "gm") return;
+  if(message.channel.type === "dm") return;
 
-
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-      prefixes: botconfig.prefix
-    };
-  }
-
-  let prefix = prefixes[message.guild.id].prefixes;
+  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot, message, args);
-
+if(commandfile) commandfile.run(bot, message, args);
 
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(botconfig.token);
