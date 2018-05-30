a78481bdcdf143c6b559d69d52ae9c0d373fc9c5
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-30 21:21:41 +0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -6,8 +6,6 @@ module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
     .setColor("#ce0c5a")
     .setTitle(bot.user.username)
-    .addField("Name", bot.user.username)
-    .addField("Prefix", "t." )
     .setThumbnail("https://image.ibb.co/bFFFon/bot.png")
     .addField("Created At", bot.user.createdAt)
     .addField("Joined At", bot.user.joinedAt)
