531f9b3ee2888bf950f8d647bf50d92e6154f7e3
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-30 22:51:16 +0500

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
     let botembed = new Discord.RichEmbed()
-    .setColor("#ce0c5a")
-    .setTitle(`${message.guild.iconURL}`, message.guild.name)
+    .setColor(botconfig.purple)
+    .setTitle(message.guild.name)
     .setThumbnail(message.guild.iconURL)
     .addField("Created At", message.guild.createdAt)
     .addField("Region", message.guild.region)
     .addField("Members", message.guild.memberCount)
-    .addField("Credits", "created by HeisenBERG#6584")
-    .addField("Created on", bot.user.createdAt);
+    .addField("Credits", "created by HeisenBERG#6584");
 
     message.delete().catch(O_o=>{});
     message.channel.send(botembed);
