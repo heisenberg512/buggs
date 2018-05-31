eff4b2618dfbd95b03438ee2f487cb1146a3b0eb
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-31 07:43:16 +0500

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   let commandEmbed = new Discord.RichEmbed()
   .setTitle("Commands")
   .addField("Roll", " t.roll (Rolls a dice)")
-  .addField("eans", " t.eans [proof] sends your screenshot/proof to event officers")
+  .addField("nickname", "set a different nickname for users")
   .addField("doggy", "sends random picture of doggy everytime user enters the command")
   .addField("avatar", "t.avatar, +avatar [user] send the avatar of author or the user tagged to channel")
   .addField("info", "displays information about the server.")
