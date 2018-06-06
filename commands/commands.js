7d72289cd7ed6d78765a21fb8906afa71595ed47
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-06 07:52:28 +0500

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
   .addField("Roll", " t.roll (Rolls a dice)")
   .addField("nickname", "set a different nickname for users")
   .addField("doggy", "sends random picture of doggy everytime user enters the command")
-  .addField("avatar", "t.avatar, +avatar [user] send the avatar of author or the user tagged to channel")
+  .addField("avatar", "sends the avatar of author or the user tagged to channel")
   .addField("info", "displays information about the server.")
   .addField("stats", "displays the date you joined this server")
   .addField("weather", "displays weather forcast of the location entered")
