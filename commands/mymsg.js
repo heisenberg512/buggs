2e3b474726518419e969e921d465e3fe9e50cddc
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-30 17:56:06 +0500

    Update mymsg.js

--- commands/mymsg.js
+++ commands/mymsg.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-  let organizer = message.guild.roles.find("name", "Head Event Organizer");
-  if(!message.member.roles.has(organizer.id)) return message.channel.send("Permission Denied").then(msg => msg.delete(5000));
+  if(message.author.id !== "332134348725813248") return message.delete().catch(O_o=>{});
   let msg = args.join(" ");
   message.delete().catch(O_o => {});
   message.channel.send(msg);
