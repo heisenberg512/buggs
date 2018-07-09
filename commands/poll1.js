2c19b28f5fdd3238c45c9b1aa5ac9918f6115e1d
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-09 14:24:50 +0000

    Update poll1.js

--- commands/poll1.js
+++ commands/poll1.js
@@ -3,17 +3,17 @@ const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
   let text = args.join(" ");
-  let admin = message.guild.roles.find("name", "administrator");
 //  if(!message.author.hasPermission("ADMINISTRATOR")) return message.delete().catch(O_o=>{});
   if(!args[0]) return message.channel.send("Usage: poll <question>", {code:'py'});
-  if(!message.author.roles.has(admin.id)) return message.delete().catch(O_o=>{});
   
   let embed = new Discord.RichEmbed()
   .setTitle(`**Poll created by ${message.author.username}**`)
   .setDescription(text)
   .setColor(botconfig.yellow);
   
-  message.channel.send(embed);
+  let msg = message.channel.send(embed);
+  await msg.react('✅');
+  await msg.react('❌');
   message.delete().catch(O_o=>{});
 }
 
