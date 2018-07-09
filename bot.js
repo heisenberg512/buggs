3dfd8080f4fcf91ab6d2ed80ae71be927844f08e
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-09 09:09:58 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,16 +8,16 @@ fs.readdir("./commands/", (err, files) => {
   if (err) console.log(err);
 
   let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands");
+ if(jsfile.length <= 0){
+   console.log("Couldn't find commands");
     return;
   }
 
-  jsfile.forEach((f, i) =>{
+ /* jsfile.forEach((f, i) =>{
   let props = require(`./commands/${f}`);
   console.log(`${f} loaded!`);
   bot.commands.set(props.help.name, props);
-    });
+    });*/
 
 });
 
