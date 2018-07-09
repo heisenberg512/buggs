f039cac77bf0885f66ffd40599c59b5362255943
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-09 13:53:51 +0000

    Rename poll.js to poll1.js

---
+++ commands/poll1.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+  let text = args.join(" ");
+  let admin = message.guild.roles.find("name", "administrator");
+  if(!message.author.roles.has(admin.id)) return message.delete().catch(O_o=>{});
+//  if(!message.author.hasPermission("ADMINISTRATOR")) return message.delete().catch(O_o=>{});
+  if(!args[0]) return message.channel.send("Usage: poll <question>", {code:'py'});
+  
+  let embed = new Discord.RichEmbed()
+  .setTitle(`**Poll created by ${message.author.username}**`)
+  .setDescription(text)
+  .setColor(botconfig.yellow);
+  
+  message.channel.send(embed);
+  message.delete().catch(O_o=>{});
+}
+
+module.exports.help = {
+name: "poll"
+}
