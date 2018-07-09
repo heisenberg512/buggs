7b3708bcf28ba2f6ab3f653c8e8e160e5e5e327d
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-09 08:29:01 +0000

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+   let text = args.join(" ");
+  if(!args[0]) return message.channel.send("Usage: poll <question>", {code:'py'});
+  let admin = message.guild.roles.find("name", "administrator"):
+  if(!message.author.has("ADMINISTRATOR")) return message.channel.send("Permission Denied").then(msg => msg.delete(1000));
+   
+   let embed = new Discord.RichEmbed()
+   .setTitle(`**poll created by ${message.author.username}`**)
+   .setFooter(" Vote either yes or no ")
+   .setDescription(text)
+   .setColor(botconfig.orange);
+   
+   message.channel.send(embed);
+   await message.react('white_check_mark');
+   await message.react('negative_squared_cross_mark');
+   
+   message.delete().catch(O_o=>{});
+
+}
+
+module.exports.help = {
+  name: "poll"
+}
