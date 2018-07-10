16a6bae16c7b589f5b177f4617355b89c55ec169
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-10 11:44:13 +0500

    Update and rename poll1.js to poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,8 +3,10 @@ const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args, tools) => {
   let text = args.join(" ");
-//  if(!message.author.hasPermission("ADMINISTRATOR")) return message.delete().catch(O_o=>{});
   if(!args[0]) return message.channel.send("Usage: poll <question>", {code:'py'});
+  let admin = message.guild.roles.find("name", "Administrator");
+  let leader = message.guild.roles.find("name", "Leader");
+  if(!message.author.roles.has(admin.id || leader.id)) return  message.delete().catch(O_o=>{});
   
   let embed = new Discord.RichEmbed()
   .setTitle(`**Poll created by ${message.author.username}**`)
