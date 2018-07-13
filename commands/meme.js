cae928b7f0a8acda1a4199432f787c65b6188538
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-13 22:17:34 +0500

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,20 @@
+const Discrod = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+   let chance = Match.floor(Math.random() * 10);
+   
+   if(chance > 1 && chance < 3){
+     message.reply("Your mama is beautiful as always");
+   }else if(chance > 3 && chance < 6){
+   message.reply("Tell your mom to shave her armpit");
+   }else{
+   message.reply("Your mom is a slut");
+   }
+   
+   message.delete().catch(O_o=>{});
+}
+
+module.exports.help = {
+ name: "meme"
+}
